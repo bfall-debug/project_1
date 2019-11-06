6e64fb34ef667c7d2b36b370ec873b9f7c63cd1d
BFall-Debug <bfall92@gmail.com>
2019-11-06 14:50:59 -0500

    genre specific querying working and general updates

--- assets/javascript/dataFunctions.js
+++ assets/javascript/dataFunctions.js
@@ -1,3 +1,7 @@
+var TmApiKey = "apikey=c5IiGTZWs4t9H3rW0Nx4pFCbT5Koq6NK";
+var TmQuery = `https://app.ticketmaster.com/discovery/v2/events.json?${TmApiKey}`;
+
+
 $("#submitBtn").on("click", function(event){
   event.preventDefault();
     navigator.geolocation.getCurrentPosition(function (position) {
@@ -6,12 +10,14 @@ $("#submitBtn").on("click", function(event){
     });
 });
 
-function ticketmasterAPI(geoHash){
+function ticketmasterAPI(geoHashCoords){
     latlong = "40.36-70.67";
-    var apiKey = "c5IiGTZWs4t9H3rW0Nx4pFCbT5Koq6NK";
-    var genre = getGenre();
-    var radius = 100
-    var url = `https://app.ticketmaster.com/discovery/v2/events.json?apikey=${apiKey}${genre}&geoPoint=${geoHash}&radius=${radius}`
+    var geoHash = `&geoPoint=${geoHashCoords}`
+    var radius = `&radius=${100}`
+    var genre = `&classificationId=${getGenre()}`
+    var type = "&classificationName=music";
+    
+    var url = TmQuery + `${genre}${geoHash}${radius}${type}`
     $.ajax({
         url: url,
         method: "GET",
@@ -27,14 +33,13 @@ function ticketmasterAPI(geoHash){
 
 function getGenre(){
     var selectedGenre = $(".genre-dropdown")[0].value
-    var genreString = ""
+    var genreId = ""
     for(var i = 0; i < genres.length; i++){
       if(selectedGenre == genres[i][0]){
         var genreId = genres[i][1];
-        genreString = `&classificationId=${genreId}`
       }
     }
-    return genreString
+    return genreId
 }
 
 
