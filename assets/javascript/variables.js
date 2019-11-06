c795102d249e1fe2e9c39d7944a538d23608f452
Kyle Jones <kylejonesnh@gmail.com>
2019-11-05 21:22:34 -0500

    added options for genres and cities

--- assets/javascript/variables.js
+++ assets/javascript/variables.js
@@ -128,3 +128,17 @@ var stateCities = [
     "WI - Milwaukee", 
     "WV - West Virginia", 
     "WY - Wyoming"]
+     
+var sel = $('.genre-dropdown');
+for(var i = 0; i < genres.length; i++) {
+    var opt = $('<option>');
+    opt.text(genres[i][0]);
+    sel.append(opt);
+}
+
+var citySel = $('.location-dropdown');
+for(var i = 0; i < stateCities.length; i++) {
+    var opti = $('<option>');
+    opti.text(stateCities[i]);
+    citySel.append(opti);
+}
